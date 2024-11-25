c25b1e82fd5a553b6a4f2338972bf751e824a513
m-shoaibdev <shoaibaas99@gmail.com>
2024-11-25 22:16:11 +0500

    Content component pass dynamic data using Props. pass data parent to child content component and difine types and condition.

--- NextJS/Assignments/assignment-03/src/app/page.tsx
+++ NextJS/Assignments/assignment-03/src/app/page.tsx
@@ -1,10 +1,30 @@
+import Content from "@/components/Content";
 import HeroSection from "@/components/Hero-section";
+import RoundCircle from "../public/WorkTogetherImage.png";
 
 export default function Home() {
   return (
     <>
     <HeroSection />
-    <h1>main content</h1>
+    <div className="bg-white pt-8 md:pt-20 lg:pt-24">
+      <Content heading="Project Management" paragraph="Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note." textColor="dark" btn="Get Started" alignImage="right"/>
+    </div>
+
+    <div className="bg-white pb-5 md:pb-8">
+      <Content heading="Work together" paragraph="With whitepace, share your notes with your colleagues and collaborate on them. You can also publish a note to the internet and share the URL with others." btn="Try it now" alignImage="left" imgUrl={RoundCircle} textColor="dark"/>
+    </div>
+
+    <div className="py-8 md:py-20 lg:py-24">
+      <Content heading="Use as Extension" paragraph="Use the web clipper extension, available on Chrome and Firefox, to save web pages or take screenshots as notes."  btn="Lets Go" alignImage="right" />
+    </div>
+
+    <div className="bg-white py-8 md:py-20 lg:py-24">
+      <Content heading="Customise it to your needs" paragraph="Customise the app with plugins, custom themes and multiple text editors (Rich Text or Markdown). Or create your own scripts and plugins using the Extension API." textColor="dark" btn="Lets go" alignImage="left"/>
+    </div>
+
+    <div className="py-8 md:py-20 lg:py-24">
+      <Content heading="Your work, everywhere you are" paragraph="Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace, Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!" btn="Try Taskey" imgBox={false}/>
+    </div>
     </>
   );
 }
---
+++ NextJS/Assignments/assignment-03/src/components/Content.tsx
@@ -0,0 +1,46 @@
+import Image, { StaticImageData } from "next/image";
+
+interface IContent {
+    alignImage?: string,
+    heading: string,
+    paragraph: string,
+    textColor?: string,
+    btn: string,
+    imgUrl?: string | StaticImageData,
+    imgBox?: boolean
+}
+
+export default function Content( {alignImage, heading, paragraph, textColor, btn, imgUrl, imgBox}: IContent ) {
+    return (
+        <div className="md:container md:mx-auto px-4 md:flex items-center gap-10 lg:gap-12">
+            
+            {imgBox === false ? "" : alignImage === "left" &&
+            <div className="flex-1 py-10">
+                <div className={ imgUrl ? "p-4" : "bg-secondary p-4 min-h-[400px] md:h-[500px] lg:h-[540px]"}>
+                    {imgUrl && <Image src={imgUrl} alt={heading} />}
+                </div>
+            </div> }
+
+            <div className={imgBox === false ? "text-center flex-1 py-10" : `text-${textColor} flex-1 py-10`}>
+              <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold">{heading}</h1>
+                <p className="text-base md:text-lg font-normal my-8 md:mt-10 md:mb-14">
+                    {paragraph}
+                </p>
+              <button className="bg-primary text-white rounded-[6px] px-4 py-3 lg:px-6 lg:py-4 text-base md:text-lg inline-flex items-center my-2">
+                {btn}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"     className="w-4 h-4 ml-1.5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                </svg>
+              </button>
+            </div>
+
+            {imgBox === false ? "" : alignImage === "right" &&
+            <div className="flex-1 py-10">
+                <div className={ imgUrl ? "p-4" : "bg-secondary p-4 min-h-[400px] md:h-[500px] lg:h-[540px]"}>
+                    {imgUrl && <Image src={imgUrl} alt={heading} />}
+                </div>
+            </div> }
+            
+        </div>
+    );
+}
